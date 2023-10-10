38d3b9daa41f8ddc17dee06bbb73c4c7dbc69821
Cooper Ry Lees <me@cooperlees.com>
2023-10-09 17:11:57 -0700

    Move to 0.7.1 + update docs

--- docs/implementors/core/cmp/trait.Ord.js
+++ docs/implementors/core/cmp/trait.Ord.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"monitord":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.71.1/core/cmp/trait.Ord.html\" title=\"trait core::cmp::Ord\">Ord</a> for <a class=\"enum\" href=\"monitord/json/enum.JsonFlatValue.html\" title=\"enum monitord::json::JsonFlatValue\">JsonFlatValue</a>"]]
+"monitord":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.72.1/core/cmp/trait.Ord.html\" title=\"trait core::cmp::Ord\">Ord</a> for <a class=\"enum\" href=\"monitord/json/enum.JsonFlatValue.html\" title=\"enum monitord::json::JsonFlatValue\">JsonFlatValue</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
