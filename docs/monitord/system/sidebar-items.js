4a63debffc9555a63f1c1efb90b46fcc5c01e5ec
Cooper Ry Lees <me@cooperlees.com>
2023-10-13 12:34:48 -0700

    Move to 0.8.0 + update docs for release

---
+++ docs/monitord/system/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"enum":["SystemdSystemState"],"fn":["get_system_state"]};
--- docs/source-files.js
+++ docs/source-files.js
@@ -1,4 +1,4 @@
 var sourcesIndex = JSON.parse('{\
-"monitord":["",[["dbus",[],["mod.rs","networkd.rs","systemd.rs","units.rs"]]],["json.rs","lib.rs","logging.rs","networkd.rs","pid1.rs","units.rs"]]\
+"monitord":["",[["dbus",[],["mod.rs","networkd.rs","systemd.rs","units.rs"]]],["json.rs","lib.rs","logging.rs","networkd.rs","pid1.rs","system.rs","units.rs"]]\
 }');
 createSourceSidebar();
