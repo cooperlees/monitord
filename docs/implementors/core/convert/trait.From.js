0df90b0a1605f9c426c66f5b620af7089fa028bf
Cooper Ry Lees <me@wcooperlees.com>
2023-12-14 18:20:22 +0000

    Move to 0.8.2 + update docs for release

--- docs/implementors/core/convert/trait.From.js
+++ docs/implementors/core/convert/trait.From.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"monitord":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.73.0/core/convert/trait.From.html\" title=\"trait core::convert::From\">From</a>&lt;<a class=\"enum\" href=\"monitord/logging/enum.LogLevels.html\" title=\"enum monitord::logging::LogLevels\">LogLevels</a>&gt; for LevelFilter"]]
+"monitord":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.74.1/core/convert/trait.From.html\" title=\"trait core::convert::From\">From</a>&lt;<a class=\"enum\" href=\"monitord/logging/enum.LogLevels.html\" title=\"enum monitord::logging::LogLevels\">LogLevels</a>&gt; for LevelFilter"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
