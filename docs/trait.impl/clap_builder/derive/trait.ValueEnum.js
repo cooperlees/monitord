8bdb58ce1bcdd968a49a05ca681e46055ba08fbc
Cooper Ry Lees <me@cooperlees.com>
2024-05-31 14:24:46 -0700

    Add new docs files I missed - been awhile since I've done OSS rust

---
+++ docs/trait.impl/clap_builder/derive/trait.ValueEnum.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"monitord":[["impl ValueEnum for <a class=\"enum\" href=\"monitord/logging/enum.LogLevels.html\" title=\"enum monitord::logging::LogLevels\">LogLevels</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
---
+++ docs/trait.impl/core/cmp/trait.PartialOrd.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"monitord":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.78.0/core/cmp/trait.PartialOrd.html\" title=\"trait core::cmp::PartialOrd\">PartialOrd</a> for <a class=\"enum\" href=\"monitord/json/enum.JsonFlatValue.html\" title=\"enum monitord::json::JsonFlatValue\">JsonFlatValue</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
