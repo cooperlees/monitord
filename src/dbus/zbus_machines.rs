ee2d2c0c5b373a4731d168d2eafd53b2b344a832
Carlos Fernandez Sanz <carlos@sanz.dev>
2025-07-07 14:00:06 -0400

    zbus_machines: Add missing import
    ```
    error[E0412]: cannot find type `MachineProxyBlocking` in this scope
      --> third-party/rust/vendor/monitord-0.14.1/src/dbus/zbus_machines.rs:36:1
       |
    36 | / #[proxy(
    37 | |     interface = "org.freedesktop.machine1.Manager",
    38 | |     default_service = "org.freedesktop.machine1",
    39 | |     default_path = "/org/freedesktop/machine1"
    40 | | )]
       | |  ^
       | |  |
       | |__not found in this scope
       |    in this procedural macro expansion
      --> third-party/rust/vendor/zbus_macros-5.7.1/src/lib.rs:197:1
       |
       = note: in this expansion of `#[proxy]`
       |
    help: consider importing this struct
       |
    22 + use crate::dbus::zbus_machine::MachineProxyBlocking;
       |
    ```

--- src/dbus/zbus_machines.rs
+++ src/dbus/zbus_machines.rs
@@ -24,6 +24,7 @@ use zbus::proxy;
 use zbus::zvariant::Type;
 
 use crate::dbus::zbus_machine::MachineProxy;
+use crate::dbus::zbus_machine::MachineProxyBlocking;
 
 #[derive(Deserialize_tuple, PartialEq, Eq, Debug, Type)]
 pub struct ListedMachine {
